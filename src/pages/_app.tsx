ccee2e7db1391951d7c9f431b76b5f0190e26285
tasuku-lab280 <56028684+tasuku-lab280@users.noreply.github.com>
2025-03-19 16:53:28 +0000

    feat: auth0の導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ""}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ""}
+      authorizationParams={{
+        redirect_uri: process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI,
+      }}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
